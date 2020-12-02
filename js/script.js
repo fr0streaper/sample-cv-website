e2101a023ed5f91c146391947f6ace202ece2910
Andrew Kashukov <andrei1999kashukov@gmail.com>
2020-12-03 00:31:52 +0300

    Add task 6

--- js/script.js
+++ js/script.js
@@ -1,88 +1,40 @@
-function partition(arr, left, right) {
-    let mid = arr[Math.floor((left + right) / 2)];
+let popupContainer = document.querySelector(".contacts-popup").parentElement;
 
-    while (left <= right) {
-        while (arr[left] < mid) {
-            ++left;
-        }
-        while (arr[right] > mid) {
-            --right;
-        }
-
-        if (left >= right) {
-            break;
-        }
-
-        [arr[left], arr[right]] = [arr[right], arr[left]];
-        ++left;
-        --right;
-    }
-
-    return right;
+function showContactsPopup() {
+    popupContainer.classList.remove("hidden");
 }
 
-function arrMin(arr) {
-    return arr.reduce((a, b) => (a > b ? b : a));
+function hideContactsPopup() {
+    popupContainer.classList.add("hidden");
 }
 
-function arrMax(arr) {
-    return arr.reduce((a, b) => (a < b ? b : a));
+let galleryViewport = document.querySelector(".gallery-viewport");
+
+function showLargeImagePreview(image) {
+    let box = image.getBoundingClientRect();
+
+    galleryViewport.style.display = "block";
+    galleryViewport.src = image.src;
+
+    let left = box.left + image.width + 10 + window.pageXOffset + "px";
+    let top = box.top + (image.height / 2) - (galleryViewport.height / 2) + window.pageYOffset + "px";
+
+    galleryViewport.style.top = top;
+    galleryViewport.style.left = left;
 }
 
-function arrMedian(arr) {
-    let vals = arr.slice();
-
-    let left = 0, right = vals.length - 1;
-    let halflen = Math.floor(vals.length / 2);
-    while (true) {
-        let mid = partition(vals, left, right);
-
-        if (mid == halflen) {
-            return vals[mid];
-        }
-        else if (mid > halflen) {
-            right = mid;
-        }
-        else {
-            left = mid + 1;
-        }
-    }
+function hideLargeImagePreview() {
+    galleryViewport.style.display = "none";
 }
 
-function quicksort(arr, left = -1, right = -1) {
-    if (left === -1 || right === -1) {
-        left = 0, right = arr.length - 1;
-    }
-    if (right <= left) {
-        return;
-    }
-
-    let mid = partition(arr, left, right);
-    quicksort(arr, left, mid);
-    quicksort(arr, mid + 1, right);
+function addImagePreviewListener(image) {
+    image.addEventListener("mouseenter", () => {
+        showLargeImagePreview(image);
+    });
+    image.addEventListener("mouseleave", () => {
+        hideLargeImagePreview();
+    });
 }
 
-let size = 10;
-let values = Array.from({ length: size }, (_, __) => Math.round(Math.random() * 2000));
-
-console.log(values);
-
-console.log(arrMin(values));
-console.log(arrMax(values));
-console.log(arrMedian(values));
-
-console.log(values.sort((a, b) => (a - b)));
-quicksort(values);
-console.log(values);
-
-let descendants = document.body.querySelectorAll("*");
-let tags = {};
-descendants.forEach((e) => {
-    if (typeof tags[e.tagName] !== "undefined") {
-        ++tags[e.tagName];
-    }
-    else {
-        tags[e.tagName] = 1;
-    }
-});
-console.log(tags);
+let galleryItems = Array.from(document.querySelector(".gallery").children);
+galleryItems.forEach(item => addImagePreviewListener(item.children[0]));
